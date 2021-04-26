e1dd7f468333aa556cb9a375daf12815f350a011
crbeltrancas <54847565+crbeltrancas@users.noreply.github.com>
2021-04-26 15:18:37 +0200

    Update task.php

--- app/models/task.php
+++ app/models/task.php
@@ -1,3 +1,9 @@
+<!-- PHP Programming Project.
+-- Our Group Members are:
+-- Aghaei Mehdi (Neptun code: L2MRA1)
+-- Beltran Castro Carlos Ricardo (Neptun code: VY6V8S)
+-- Purevsuren Davaadorj (Neptun code: CYLJ5C) -->
+
 <?php
 require_once 'db.php';
 require_once 'session.php';
