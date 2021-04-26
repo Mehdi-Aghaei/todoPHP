7d0d487ea5d261c967fad52b695eed9615e45d23
crbeltrancas <54847565+crbeltrancas@users.noreply.github.com>
2021-04-26 15:30:02 +0200

    Update footer.php

--- app/views/footer.php
+++ app/views/footer.php
@@ -1,8 +1,2 @@
-<!-- PHP Programming Project.
--- Our Group Members are:
--- Aghaei Mehdi (Neptun code: L2MRA1)
--- Beltran Castro Carlos Ricardo (Neptun code: VY6V8S)
--- Purevsuren Davaadorj (Neptun code: CYLJ5C) -->
-
 <script src="app/views/js/jquery-3.1.1.min.js"></script>
 <script src="app/views/js/main.js"></script>
